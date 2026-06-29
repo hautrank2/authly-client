1df4cc297c4ca9e207dacc9e478acb10c60bef9a
Hậu Trần <hautrantrung.02@gmail.com>
2026-06-29 16:18:27 +0700

    depl(Deploy.sh): modify docker compose

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,8 @@ git reset --hard origin/main
 
 # Build and run Docker containers
 echo "🐳 Building and starting Docker containers..."
-docker-compose down || true
-docker-compose up -d --build
+docker compose down || true
+docker compose up -d --build
 
 echo "✅ Deployment completed successfully!"
 echo "🌐 App is running on port 3004"
